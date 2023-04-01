8bac7c6ea664427d4351415768fe489250dd44ee
Ryan Moore <ryanm.inbox@gmail.com>
2023-04-01 18:32:35 -0400

    fix alias echo and working check if clunk.py exists

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+if [ ! -e "clunk.py" ]; then
+  echo "Error: clunk.py file not found in $(pwd)" >&2
+  return 1
+fi
+
 chmod +x clunk.py
 
-# Add the clunk command to your system path
 echo 'export PATH="$PATH:'"$PWD"'"' >> ~/.bash_profile
-echo 'alias clunk="clunk.py" >> ~/.bash_profile'
+echo 'alias clunk="clunk.py"' >> ~/.bash_profile
 source ~/.bash_profile
 
-echo "clunk command installed successfully!"
+echo "clunk installed successfully!"
